808d3ffac4866333b29f670ebb7d2cf2f02020cc
mosesWigglesworth <moseswigglesworth@gmail.com>
2025-01-27 21:42:14 +0530

    Included pom.xml and other miscellaneous files

---
+++ admin-server-ms/src/test/java/com/stc/admin/server/AdminServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stc.admin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
