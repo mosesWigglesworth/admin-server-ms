d71c8f3bde7078542f63ebffa52602f611226417
mosesWigglesworth <moseswigglesworth@gmail.com>
2025-02-03 05:32:34 +0530

    Included Packaging as WAR

--- src/main/java/com/stc/admin/server/AdminServerMsApplication.java
+++ src/main/java/com/stc/admin/server/AdminServerMsApplication.java
@@ -1,16 +1,34 @@
 package com.stc.admin.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+//@SpringBootApplication
+//@EnableAdminServer
+//public class AdminServerMsApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AdminServerMsApplication.class, args);
+//	}
+//
+//}
+
 @SpringBootApplication
 @EnableAdminServer
-public class AdminServerMsApplication {
+public class AdminServerMsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminServerMsApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
